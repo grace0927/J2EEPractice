b139ff40bec87bdd5af74c566b2355f72e8c7129
grace0927 <cellphone2008@gmail.com>
2016-02-12 17:04:49 -0500

    init repo
    add repo for j2ee practice

---
+++ src/main/java/us/fengs/j2eepractice/App.java
@@ -0,0 +1,13 @@
+package us.fengs.j2eepractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
